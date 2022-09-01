1d9b62f9c3b49d5c7961b5736dc8eabb1de9bb96
Wesley Jacoby <wesleyjacoby@gmail.com>
2022-09-01 12:42:51 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# Frontend Mentor - Password Generator App Solution
+
+This is a solution to the [Password generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/password-generator-app-Mr8CLycqjh).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Generate a password based on the selected inclusion options.
+- Copy the generated password to the computer's clipboard.
+- See a strength rating for their generated password.
+- View the optimal layout for the interface depending on their device's screen size.
+- See hover and focus states for all interactive elements on the page.
+
+### Screenshot
+
+![](./assets/images/password-generator-app-desktop.png)
+![](./assets/images/password-generator-app-desktop-weak.png)
+![](./assets/images/password-generator-app-mobile.png)
+![](./assets/images/password-generator-app-mobile-error.png)
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/wesleyjacoby/Password-Generator)
+- Live Site URL: [GitHub Pages](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- JavaScript
+- zxcvbn (Password Strength Library)
+
+
+### What I learned
+
+Where do I start! This challenge was way above my skill level, but the app just looked really cool and I couldn't resist trying it out. I thought it would be a great way to practice using JavaScript arrays, but I found the CSS way more challenging!
+
+I learnt that not all browsers are created equally! The app looks good on Chrome, Firefox and Edge now.
+
+Styling the slider was a huge headache, especially getting the background behind the thumb a different color to the background ahead of the thumb. This was the code that got it working:
+
+```js
+numberOutput.innerText = slider.value;
+
+slider.oninput = function () {
+    numberOutput.textContent = this.value;
+}
+
+setBackgroundSize(input);
+
+function setBackgroundSize(input) {
+    input.style.setProperty("--background-size", `${getBackgroundSize(input)}%`);
+}
+
+input.addEventListener("input", () => setBackgroundSize(input));
+
+function getBackgroundSize(input) {
+    const min = +input.min || 1;
+    const max = +input.max || 20;
+    const value = +input.value;
+
+    const size = (value - min) / (max - min) * 100;
+
+    return size;
+}
+```
+
+Using the zxcvbn library was a huge help and time saver and made the project more realy world friendly:
+
+```html
+<script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.2.0/zxcvbn.js"></script>
+```
+
+I find myself getting more comfortable using functions as well.
+
+### Continued development
+
+There's obviously still an insane amount to learn, but I'll keep doing more challenges so I can be presented with a multiple of different problems to try solve.
+
+### Useful resources
+
+There's just too many to list here. I spent an entire day trying to style that slider. The code above shows how I finally got it to work.
+
+## Author
+
+- Frontend Mentor - [@wesleyjacoby](https://www.frontendmentor.io/profile/yourusewesleyjacobyrname)
---
+++ app.js
@@ -0,0 +1,197 @@
+let slider = document.querySelector('#slider');
+let numberOutput = document.querySelector('.character-amount');
+
+const passwordArea = document.querySelector('.password-text');
+const copyGraphic = document.querySelector('.copy');
+const copiedText = document.querySelector('.copied-text');
+
+const input = document.querySelector("input");
+const form = document.querySelector('.form-section');
+
+const uppercaseCheckbox = document.querySelector('#uppercase');
+const lowercaseCheckbox = document.querySelector('#lowercase');
+const numbersCheckbox = document.querySelector('#numbers');
+const symbolsCheckbox = document.querySelector('#symbols');
+const checkboxContainer = document.querySelector('.checkbox-container:nth-of-type(4)');
+
+const error = document.querySelector('.error');
+
+const strengthWord = document.querySelector('.strength-word');
+const tooWeak = document.querySelector('#too-weak');
+const weak = document.querySelector('#weak');
+const medium = document.querySelector('#medium');
+const strong = document.querySelector('#strong');
+
+
+const uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '<', '>', '/', '-', '+'];
+
+
+// --SLIDER FUNCTIONALITY--
+
+numberOutput.innerText = slider.value;
+
+slider.oninput = function () {
+    numberOutput.textContent = this.value;
+}
+
+setBackgroundSize(input);
+
+function setBackgroundSize(input) {
+    input.style.setProperty("--background-size", `${getBackgroundSize(input)}%`);
+}
+
+input.addEventListener("input", () => setBackgroundSize(input));
+
+function getBackgroundSize(input) {
+    const min = +input.min || 1;
+    const max = +input.max || 20;
+    const value = +input.value;
+
+    const size = (value - min) / (max - min) * 100;
+
+    return size;
+}
+
+// -- SLIDER FUNCTIONALITY--
+
+function getMasterArray() {
+    masterArray = [];
+
+    if (uppercaseCheckbox.checked === true) {
+        masterArray.push(...uppercaseArray);
+    }
+    if (lowercaseCheckbox.checked === true) {
+        masterArray.push(...lowercaseArray);
+    }
+    if (numbersCheckbox.checked === true) {
+        masterArray.push(...numbersArray);
+    }
+    if (symbolsCheckbox.checked === true) {
+        masterArray.push(...symbolsArray);
+    }
+    return masterArray;
+}
+
+const chooseRandom = (masterArray, num) => {
+
+    const password = [];
+    for (let i = 0; i < num;) {
+        const random = Math.floor(Math.random() * masterArray.length);
+        if (password.indexOf(masterArray[random]) !== -1) {
+            continue;
+        };
+        password.push(masterArray[random]);
+        i++;
+    };
+
+    return password;
+};
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (uppercaseCheckbox.checked !== true && lowercaseCheckbox.checked !== true && numbersCheckbox.checked !== true && symbolsCheckbox.checked !== true) {
+        error.style.display = 'block';
+        checkboxContainer.style.marginBottom = '16px';
+        return error.innerText = 'Please select a checkbox above';
+    } else {
+        error.style.display = 'none';
+        checkboxContainer.style.marginBottom = '32px';
+    }
+
+    password = chooseRandom(getMasterArray(), slider.value);
+    const passwordString = password.join('');
+    const passwordStrength = zxcvbn(passwordString).score;
+
+    checkPasswordStrength(passwordStrength);
+
+    passwordArea.innerText = passwordString;
+    passwordArea.style.opacity = '1';
+})
+
+copyGraphic.addEventListener('click', () => {
+    const copyPassword = passwordArea.innerText;
+
+    navigator.clipboard.writeText(copyPassword);
+    copiedText.innerText = 'copied';
+    passwordArea.innerText = 'P4$5W0rD!';
+    passwordArea.style.opacity = '0.25';
+
+    setTimeout(() => {
+        copiedText.innerText = '';
+    }, 3000)
+
+    form.reset();
+    setBackgroundSize(input);
+    numberOutput.textContent = '10';
+    resetBars();
+})
+
+function checkPasswordStrength(passwordStrength) {
+
+    resetBars();
+
+    switch (passwordStrength) {
+        case 0:
+            strengthWord.innerText = 'too weak!';
+            tooWeak.style.border = 'none';
+            tooWeak.style.backgroundColor = 'hsl(0, 91%, 63%)';
+            break;
+        case 1:
+            strengthWord.innerText = 'too weak!';
+            tooWeak.style.border = 'none';
+            tooWeak.style.backgroundColor = 'hsl(0, 91%, 63%)';
+            break;
+        case 2:
+            strengthWord.innerText = 'weak';
+            tooWeak.style.border = 'none';
+            weak.style.border = 'none';
+            tooWeak.style.backgroundColor = 'hsl(13, 95%, 66%)';
+            weak.style.backgroundColor = 'hsl(13, 95%, 66%)';
+            break;
+        case 3:
+            strengthWord.innerText = 'medium';
+            tooWeak.style.border = 'none';
+            weak.style.border = 'none';
+            medium.style.border = 'none';
+            tooWeak.style.backgroundColor = 'hsl(42, 91%, 68%)';
+            weak.style.backgroundColor = 'hsl(42, 91%, 68%)';
+            medium.style.backgroundColor = 'hsl(42, 91%, 68%)';
+            break;
+        case 4:
+            strengthWord.innerText = 'strong';
+            tooWeak.style.border = 'none';
+            weak.style.border = 'none';
+            medium.style.border = 'none';
+            strong.style.border = 'none';
+            tooWeak.style.backgroundColor = 'hsl(127, 100%, 82%)';
+            weak.style.backgroundColor = 'hsl(127, 100%, 82%)';
+            medium.style.backgroundColor = 'hsl(127, 100%, 82%)';
+            strong.style.backgroundColor = 'hsl(127, 100%, 82%)'
+            break;
+        default:
+            error.style.display = 'block';
+            checkboxContainer.style.marginBottom = '16px';
+            error.innerText = 'Something horrific has happened';
+    }
+}
+
+function resetBars() {
+    strengthWord.innerText = '';
+
+    tooWeak.style.border = '2px solid hsl(252, 11%, 91%)';
+    tooWeak.style.backgroundColor = 'transparent';
+
+    weak.style.border = '2px solid hsl(252, 11%, 91%)';
+    weak.style.backgroundColor = 'transparent';
+
+    medium.style.border = '2px solid hsl(252, 11%, 91%)';
+    medium.style.backgroundColor = 'transparent';
+
+    strong.style.border = '2px solid hsl(252, 11%, 91%)';
+    strong.style.backgroundColor = 'transparent';
+}
