63fd0fb34bf5069d7936abd2e5aab2ac1098e8f5
Wesley <wesleyjacoby@gmail.com>
2022-09-16 10:19:04 +0200

    Remove some special characters

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const strong = document.querySelector('#strong');
 const uppercaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const lowercaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '<', '>', '/', '-', '+'];
+const symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '/', '-', '+'];
 
 
 // --SLIDER FUNCTIONALITY--
